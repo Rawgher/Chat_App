021f941b44d1f736429ad693a4f72bb9e41c1cea
Roger <rdpender3@gmail.com>
2019-04-04 23:45:56 -0700

    set up redux and added container

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import HomeScreen from "./components/HomeScreen";
 import SignInScreen from "./components/SignInScreen";
+import chatNowReducers from "./reducers";
+
+let store = createStore(chatNowReducers);
 
 export default class App extends Component {
   render() {
-    return <AppContainer />;
+    console.log("redux/app state is: ", store.getState());
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
   }
 }
 
---
+++ reducers/index.js
@@ -0,0 +1,28 @@
+import { combineReducers } from "redux";
+
+function name(state = "", action) {
+  switch (action.type) {
+    case "UPDATE_NAME":
+      return action.name;
+    default:
+      return state;
+  }
+}
+
+function accountNumber(state = "", action) {
+  switch (action.type) {
+    case "UPDATE_ACCOUNT_NUMBER":
+      return action.number;
+    default:
+      return state;
+  }
+}
+
+// always need a default: return state
+
+const chatNowReducers = combineReducers({
+  name,
+  accountNumber
+});
+
+export default chatNowReducers;
