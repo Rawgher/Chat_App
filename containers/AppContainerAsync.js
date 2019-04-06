f5b905f9cdb9bab7efec6e85bb5c05d12d62f71a
Roger <rdpender3@gmail.com>
2019-04-06 11:19:49 -0700

    function all there. can add sign in skip at later point

---
+++ containers/AppContainerAsync.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { rehydrateNameAndAccountNum } from "../actions";
+import App from "../App";
+
+const mapStateToProps = state => ({
+  name: state.name,
+  accountNumber: state.accountNumber
+});
+
+const mapDispatchToProps = dispatch => ({
+  onRehydrateFromLocalStorage: (name, accountNumber) => {
+    dispatch(rehydrateNameAndAccountNum(name, accountNumber));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
